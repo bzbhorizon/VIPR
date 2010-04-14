ca8b427f98358b0fa60451272c1d13f1811c2fd5
bzbhorizon <bzb@cs.nott.ac.uk>
2010-04-14 21:03:12 +0100

    Shifted browser index file to content dir

--- src/bzb/se/Paths.java
+++ src/bzb/se/Paths.java
@@ -16,7 +16,7 @@ public final class Paths {
 	public static final String DB_XML_FILE_URL = RES_DIR + "markers.xml";
 	public static final String DB_LOGIN_FILE_URL = RES_DIR + "db_login";
 	
-	public static final String CONTENT_INDEX_URL = RES_DIR + "index.html";
+	public static final String CONTENT_INDEX_URL = CONTENT_DIR + "index.html";
 	
 	public static final String ICONS_OVERLAY_URL = RES_DIR + "icons.kml";
 	
